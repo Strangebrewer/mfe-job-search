6257a146addc46223f92e091982c05806899813a
Strangebrewer <borrowed.carbon.atoms@gmail.com>
2025-12-28 14:52:14 -0700

    more stuff

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './index.css';
 
 const App: React.FC = () => {
   return (
-    <div className='bg-yellow-500'>Hello, Woyld from App 2!</div>
+    <div className='tw:bg-yellow-500'>Hello, Woyld from App 2!</div>
   )
 }
 
