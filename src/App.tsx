5400a6ac30418089f0d65fc74b46f5244472d8b9
Strangebrewer <borrowed.carbon.atoms@gmail.com>
2026-03-02 22:57:27 -0700

    fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, MemoryRouter } from 'react-router-dom';
 import './index.css';
+import Home from './pages/Home';
 
-const App: React.FC = () => {
+const App: React.FC<any> = () => {
   return (
-    <main className='tw:grow'>Hello, Woyld from App 2!</main>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="derp" element={<div>Hey there, Derp!</div>} />
+    </Routes>
   )
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import { useNavigate } from 'react-router-dom';
+
+const Home: React.FC = () => {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h1>Hey there, Too!</h1>
+      <button onClick={() => navigate('/stuff/derp')}>Derp!</button>
+      <br/>
+      <button onClick={() => navigate('/shizzle')}>Fake Route!</button>
+    </div>
+  )
+}
+
+export default Home;
